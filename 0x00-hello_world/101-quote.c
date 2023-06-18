05265d81684b9a7812d99634dafdc6800baf0d4e
TebogoMahloko <mahloko52@gmail.com>
2023-06-18 15:44:36 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,19 +1,12 @@
-#include <unistd.h>
-#include <sys/syscall.h>
+#include <unistd.h>#include <unistd.h>
+
 /**
- *  main - Entry point
- *
- *  Description: Prints a specific  message to standard error
- *
- *  Return: Always 1
- */
-
-int mani(void)
+* main - Prints a text
+*
+* Return: Always 1 (Success)
+*/
+int main(void)
 {
-	const char *message =
-		"and that pieace of art ix useful\" - Dora Kopar, 2015-10-19\n";
-	size_t len = 59; /* Length of the message */
-
-	syscall(SYS_write, STDERR_FILENO, message, len);
+	write(2, "and that piece of art is useful\" - Dora Korpar,2015-10-19\n", 59);
 	return (1);
 }
