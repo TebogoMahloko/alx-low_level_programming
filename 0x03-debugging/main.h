b76f2cd6839cbe387730b0db05a470feee43010b
TebogoMahloko <mahloko52@gmail.com>
2023-06-22 13:00:21 -0700

    2-largest_number.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,9 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdi.h>
+#include <stdio.h>
 
-void positive_or_negitive(int i);
+void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
